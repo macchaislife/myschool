165d9857c981bbc4dd313acbd723873b97e00f05
macchaislife <taimengsongben@gmail.com>
2026-06-07 18:08:23 +0900

    views kousin

--- home/views.py
+++ home/views.py
@@ -412,6 +412,9 @@ def lesson_question_detail(
         question.answer = request.POST.get("answer")
         question.answered_at = timezone.now()
 
+        # 回答済みにする
+        question.is_answered = True
+
         question.save()
 
         return redirect("teacher_dashboard")
@@ -423,7 +426,6 @@ def lesson_question_detail(
             "question": question
         }
     )
-
 
 # =====================================================
 # 管理者ダッシュボード
